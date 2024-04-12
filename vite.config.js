fe69eb7e5d9db09e3d2fef21841c933121bf9451
Manuel Venegas <146491066+Manualliee@users.noreply.github.com>
2024-04-12 12:32:28 -0700

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/my-portfolio-2.0/",
+  base: "/my-portfolio-2.0",
   plugins: [react()],
 })
